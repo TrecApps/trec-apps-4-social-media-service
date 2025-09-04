fc3f3e97111a8cb6ccc79a7a5b905765e774a6d2
John Jacko <jljacko@outlook.com>
2025-09-04 14:52:35 -0600

    Implemented Security Config

--- src/main/java/com/trecapps/sm/common/SecurityConfig.java
+++ src/main/java/com/trecapps/sm/common/SecurityConfig.java
@@ -1,4 +1,64 @@
 package com.trecapps.sm.common;
 
+import com.trecapps.auth.webflux.services.TrecAuthManagerReactive;
+import com.trecapps.auth.webflux.services.TrecSecurityContextReactive;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+@Slf4j
 public class SecurityConfig {
+    @Autowired
+    SecurityConfig(
+            TrecSecurityContextReactive trecSecurityContext1,
+            TrecAuthManagerReactive trecAuthManagerReactive)
+    {
+        trecSecurityContext = trecSecurityContext1;
+        this.trecAuthManagerReactive = trecAuthManagerReactive;
+    }
+    TrecSecurityContextReactive trecSecurityContext;
+    TrecAuthManagerReactive trecAuthManagerReactive;
+
+//    String[] restrictedEndpoints = {
+//            "/Notifications/*",
+//            "/Notifications/**"
+//
+//    };
+
+    String[] verifiedEndpoints = {
+            "/Profile/*",
+            "/Profile/**",
+            "/Content/**",
+            "/Content/*",
+            "/Reactions/*",
+            "/Reactions/**",
+            "/Connections/**",
+            "/Connections/*"
+    };
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(
+            ServerHttpSecurity http) {
+        log.info("Preparing Security Bean");
+
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
+                .formLogin(ServerHttpSecurity.FormLoginSpec::disable)
+                .logout(ServerHttpSecurity.LogoutSpec::disable)
+                .authorizeExchange(exchanges -> exchanges
+                        //.pathMatchers(restrictedEndpoints).authenticated()
+                        .pathMatchers(verifiedEndpoints).hasAuthority("TREC_VERIFIED")
+                        .anyExchange().permitAll())
+                .authenticationManager(trecAuthManagerReactive)
+                .securityContextRepository(trecSecurityContext)
+
+                .build();
+    }
 }
